c959faa6900c18eac2cf0add4e21ad04ab961c4d
unknown <stanj89@gmail.com>
2019-07-10 23:28:23 -0400

    mvp + accidental stretch.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const helmet = require('helmet')
 
+const postRouter = require('./posts/postRouter')
+const userRouter = require('./users/userRouter')
+
 const server = express();
 
 server.get('/', (req, res) => {
@@ -10,7 +13,21 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
+  console.log(`${req.method} to ${req.path} @ ${Date.now()}`)
 
+  next();
 };
+
+
+server.use(logger);
+server.use(helmet());
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the User Database' })
+});
+
+server.use('/api/posts', postRouter)
+server.use('/api/users', userRouter)
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,132 @@
-const express = 'express';
+const express = require('express');
+
+const Users = require('./userDb')
+const Posts = require('../posts/postDb')
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-
+//POST new user
+router.post('/', validateUser, async (req, res) => {
+    try {
+        const user = await Users.insert(req.body)
+        res.status(200).json(user)
+    } catch (err) {
+        res.status(500).json({
+            message: "User could not be created."
+        })
+    }
 });
 
-router.post('/:id/posts', (req, res) => {
+//POST new post by given user
+router.post('/:id/posts', validateUserId, validatePost, async (req, res) => {
+    try {
+        const postInfo = {
+            text: req.body.text,
+            user_id: req.user
+        }
+        const post = await Posts.insert(postInfo)
+        res.status(200).json(post)
+    } catch (err) {
+        res.status(500).json({
+            message: "Post could not be created."
+        })
+    }
+})
 
+//GET all users
+router.get('/', async (req, res) => {
+    try {
+        const users = await Users.get();
+        res.status(200).json(users)
+    } catch (err) {
+        res.status(500).json({
+            message: "Error retrieving the users."
+        })
+    }
 });
 
-router.get('/', (req, res) => {
-
+//GET specific user
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await Users.getById(req.params.id);
+        res.status(200).json(user)
+    } catch (err) {
+        res.status(500).json({
+            message: "There was an error locating the user."
+        })
+    }
 });
 
-router.get('/:id', (req, res) => {
-
+//GET all posts by a user
+router.get('/:id/posts', validateUserId, async (req, res) => {
+    try {
+        const posts = await Users.getUserPosts(req.user);
+        res.status(200).json(posts)
+    } catch (err) {
+        res.status(500).json({
+            message: "There was an error locating the user's posts."
+        })
+    }
 });
 
-router.get('/:id/posts', (req, res) => {
-
+//DELETE a user
+router.delete('/:id', validateUserId, async (req, res) => {
+    try {
+        const count = await Users.remove(req.user)
+        res.status(200).json({
+            message: `${count} user(s) deleted.`
+        })
+    } catch (err) {
+        res.status(500).json({
+            message: "There was an error deleting the user."
+        })
+    }
 });
 
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
+//PUT a user
+router.put('/:id', validateUserId, async (req, res) => {
+    try {
+        const update = await Users.update(req.user, req.body)
+        res.status(200).json(update)
+    } catch (err) {
+        res.status(500).json({
+            message: "The user could not be updated."
+        })
+    }
 });
 
 //custom middleware
 
-function validateUserId(req, res, next) {
-
-};
+async function validateUserId(req, res, next) {
+    try {
+        console.log(await Users.getById(req.params.id))
+        const user = await Users.getById(req.params.id)
+        if (user.id == req.params.id) {
+            req.user = user.id
+            next()
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The user could not be found."
+        })
+    }
+}
 
 function validateUser(req, res, next) {
+    Object.keys(req.body).length !== 0
+        ? req.body.name
+            ? next()
+            : res.status(400).json({ message: "missing name data" })
+        : res.status(400).json({ message: "missing user data" })
+}
 
-};
 
 function validatePost(req, res, next) {
-
+    Object.keys(req.body).length !== 0
+        ? req.body.text
+            ? next()
+            : res.status(400).json({ message: "missing post content" })
+        : res.status(400).json({ message: "missing post data" })
 };
 
 module.exports = router;
